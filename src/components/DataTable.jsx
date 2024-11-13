761b8b5c8c41f53a0002c67be99833309feb7bff
SyedMuhammadRehan <syedrehan55.rs@gmail.com>
2024-11-13 22:55:02 +0500

    Table design refine

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,60 +1,62 @@
-// import React, { useEffect, useState } from 'react';
-// import { DataTable } from 'primereact/datatable';
-// import { Column } from 'primereact/column';
-// import "primereact/resources/themes/lara-light-cyan/theme.css";
-
-
-// const DraggableTable = (props) => {
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     setData(props.data)
-//   },[props.data])
-
-//   const onRowReorder = (e) => {
-//     setData(e.value); // Update the data array after reordering
-//   };
-
-//   return (
-//     <div className="card">
-//       <DataTable value={data} reorderableRows onRowReorder={onRowReorder} responsiveLayout="scroll">
-//         <Column rowReorder style={{ width: '3em' }} />
-//         <Column field="id" header="Id" />
-//         <Column field="filename" header="Name" />
-//         <Column field="fileUrl" header="Url" />
-//         <Column field="fileType" header="Type" />
-//       </DataTable>
-//     </div>
-//   );
-// };
-
-// export default DraggableTable;
-
-
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import { Toast } from 'primereact/toast';
+
+import 'primereact/resources/themes/saga-blue/theme.css'; // Use a theme if not already imported
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 
 const DraggableTable = (props) => {
-  // Sample data
-  const initialData = [
-    { id: 1, name: 'Item 1', value: '100' },
-    { id: 2, name: 'Item 2', value: '200' },
-    { id: 3, name: 'Item 3', value: '300' },
-  ];
 
-  const [data, setData] = useState(initialData);
+  const [data, setData] = useState([]);
+  const toast = useRef(null);
+
+  useEffect(() => {
+    setData(props.data)
+  },[props.data])
 
   const onRowReorder = (e) => {
     setData(e.value); // Update the data array after reordering
   };
 
+  const copyToClipboard = (url) => {
+    navigator.clipboard.writeText(url);
+    toast.current.show({ severity: 'info', summary: 'Info', detail: 'Url Copied' });
+  };
+
+  const fileUrlTemplate = (rowData) => {
+    const filter = props.data.filter((info)=>info.id === rowData.id)
+    return (
+        <div className="copy-icon-container" style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+        
+        <i
+          tooltip="Confirm to proceed" tooltipOptions={{ showDelay: 1000, hideDelay: 300 }}
+          className="pi pi-copy"
+          onClick={() => copyToClipboard(rowData.fileUrl)}
+          style={{ fontSize: '1.2rem', color: '#007ad9' }}
+          title="Copy URL"
+        ></i>
+      </div>
+    );
+  };
+
   return (
-    <div className="card">
-      <DataTable value={data} reorderableRows onRowReorder={onRowReorder} responsiveLayout="scroll">
-        <Column rowReorder style={{ width: '3em' }} /> {/* Enable row reorder on the first column */}
-        <Column field="name" header="Name" />
-        <Column field="value" header="Value" />
+    <div style={{marginTop: 10}}>
+      <Toast ref={toast} />
+      <DataTable
+        value={data}
+        reorderableRows
+        onRowReorder={onRowReorder}
+        responsiveLayout="scroll"
+        className="p-datatable-custom"
+      >
+        <Column rowReorder style={{ width: '4em' }} /> {/* Enable row reorder on the first column */}
+        <Column style={{ width: '6em', padding: '8px' }} field="id" header="Id" />
+        <Column style={{ width: '12em', padding: '8px' }} field="filename" header="Name" />
+        <Column style={{ width: '15em', padding: '8px' }} body={fileUrlTemplate} header="Url" />
+        <Column style={{ width: '8em', padding: '8px' }} field="fileType" header="Type" />
       </DataTable>
     </div>
   );
