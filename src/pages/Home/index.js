06322a76089d25a01d720a807ee6d962752c680f
Nadir123649 <nadir.ali@novatoresols.com>
2024-11-13 18:08:31 +0500

    add table

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import FileUpload from '../../components/FileUpload';
 
 const Home = () => {
     return (
         <div>
-            <DndProvider backend={HTML5Backend}>
-                <FileUpload />
-            </DndProvider>
+            <FileUpload />
         </div>
     );
 }
