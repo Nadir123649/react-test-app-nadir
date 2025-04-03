ec67a8b9dc9f0de9ce290d539dbe4ddf5af1a100
Nadir123649 <nadir.ali@novatoresols.com>
2025-04-03 18:58:49 +0500

    cide commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
 import DataTable from 'react-data-table-component';
 
@@ -6,6 +6,20 @@ const Home = () => {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        const dummyData = [
+            { first_name: 'John', sur_name: 'Doe', email: 'john.doe@example.com' },
+            { first_name: 'Jane', sur_name: 'Smith', email: 'jane.smith@example.com' },
+            { first_name: 'James', sur_name: 'Brown', email: 'james.brown@example.com' },
+            { first_name: 'Emily', sur_name: 'Davis', email: 'emily.davis@example.com' },
+            { first_name: 'John', sur_name: 'Doe', email: 'john.doe@example.com' },
+            { first_name: 'Jane', sur_name: 'Smith', email: 'jane.smith@example.com' },
+            { first_name: 'James', sur_name: 'Brown', email: 'james.brown@example.com' },
+            { first_name: 'Emily', sur_name: 'Davis', email: 'emily.davis@example.com' },
+        ];
+        setUsers(dummyData);
+        setLoading(false);
+    }, []);
 
     const columns = [
         {
@@ -45,7 +59,7 @@ const Home = () => {
                     <h2 className="text-[24px] font-bold">Vessels Details!</h2>
                     <button
                         className="bg-[#FF7A32] px-4 py-2 rounded text-white font-medium hover:bg-[#E66928] transition duration-300">
-                       Add Vessel Assignment
+                        Add Vessel Assignment
                     </button>
                 </div>
                 <div className="bg-white p-4 rounded-lg">
